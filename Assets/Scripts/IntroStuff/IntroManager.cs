14423c4850a407002ef3261d2bb00d43b6572bcd
Trento <tvonlind@andrew.cmu.edu>
2021-01-30 23:44:45 -0500

    Starting work on narrative screen

---
+++ Assets/Scripts/IntroStuff/IntroManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartIntro(IntroDialogue intro)
+    {
+        Debug.Log("Starting intro");
+
+        sentences.Clear();
+
+        foreach (string sentence in intro.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndIntro();
+            return;
+        }
+
+        string setnence = sentences.Dequeue();
+    }
+
+    void EndIntro()
+    {
+        Debug.Log("End of intro");
+    }
+}
---
+++ Assets/Scripts/IntroStuff/NarrativeTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NarrativeTrigger : MonoBehaviour
+{
+    public IntroDialogue intro;
+
+    private void OnEnable()
+    {
+        TriggerDialogue();
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<IntroManager>().StartIntro(intro);
+    }
+}
